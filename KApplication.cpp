51cec2e974b06b684194029acb88932b6374bbf8
kkke <kkke@nwsuaf.edu.cn>
2017-06-19 17:58:26 +0800

    update

--- KApplication.cpp
+++ KApplication.cpp
@@ -1,6 +1,18 @@
 #include "KApplication.h"
 #include "curses.h"
 
-KApplicatioin::KApplicatioin(int argc,char **argv):argc(argc),argv(argv)
+KApplication::KApplication(int argc,char **argv):args(argv,argv+argc)
 {
+	self=this;
+	initscr();
+}
+
+std::vector<char*>& KApplication::arguments()
+{
+	return args;
+}
+
+KApplication* KApplication::instance()
+{
+	return self;
 }
--- KApplication.h
+++ KApplication.h
@@ -1,14 +1,20 @@
 #ifndef KAPPLICATION_H
 #define KAPPLICATION_H
 
-class KApplicatioin{
+#include <vector>
+
+class KApplication{
 private:
-	int argc;
-	char **argv;
+	std::vector<char*>args;
+	static KApplication *self;
 public:
-	KApplicatioin(int argc,char **argv);
-	virtual ~KApplicatioin();
+	static KApplication* instance();
+	KApplication(int argc,char **argv);
+	virtual ~KApplication();
+	std::vector<char*>& arguments();
 	int exec();
 };
+
+#define kApp KApplication::instance()
 
 #endif //KAPPLICATION_H
